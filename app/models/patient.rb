487cd5ac44cea08ed03c4935f394e10e6b754123
Paulo César Dianin Junior <maxjuniorbr@gmail.com>
2015-10-05 18:45:46 -0300

    Adicionado estrutura de lista na edição do paciente

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,9 +1,15 @@
 class Patient < ActiveRecord::Base
   belongs_to :health_plan
-  has_many :adreesses
+  has_many :addresses
+  accepts_nested_attributes_for :addresses, allow_destroy: true
 
   validates :patient, presence: true
   validates :birth, presence: true
   validates :marital_status_id, presence: true
   validates :email, presence: true
+
+  def tags_for_form
+    collection = addresses.where(patient_id: id)
+    collection.any? ? collection : addresses.build
+  end  
 end
